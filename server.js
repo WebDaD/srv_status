bad11505c1b32141993316625185106d5ae1ab22
Dominik Sigmund <dominik.sigmund@br.de>
2019-01-15 12:25:38 +0100

    Server Done and Working

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ let http = require('http')
 let fs = require('fs')
 let server = http.createServer(function (req, res) {
   if (req.method === 'GET') {
-    fs.readFile(file, function (error, data) {
+    fs.readFile(file, 'utf8', function (error, data) {
       if (error) {
         console.error(error)
-        res.writeHead(501, {'Content-Type': 'application/json'})
-        res.send(JSON.parse(error))
+        res.writeHead(501, {'Content-Type': 'text/json'})
+        res.write(error)
+        res.end()
       } else {
-        res.writeHead(200, {'Content-Type': 'application/json'})
-        res.send(JSON.parse(data))
+        res.writeHead(200, {'Content-Type': 'text/json'})
+        res.write(data)
+        res.end()
       }
     })
   }
